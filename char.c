2dd7ab2fddd2fd356843a01d559ff30f442f352c
Elverton <elvertoncf@gmail.com>
2022-09-29 14:31:32 -0300

    Removendo arquivos duplicados

--- char.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-  char x;
-  x = 1000;
-  printf("%d\n", x);
-  return 0;
-}
--- le_e_imprime.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-
-  char x = 'a';
-  printf("%d", x);
-  return 0;
-}

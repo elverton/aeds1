53fbb421aa657ce6423168ab64d2448790c18501
elverton <elvertoncf@gmail.com>
2023-02-16 16:03:30 -0300

    Atualização para 2023-1

---
+++ 2023-1/pratica/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int a;
+	int b;
+	int c;
+	printf("Digite A: ");
+	scanf("%d",&a);
+	printf("Digite B: ");
+	scanf("%d",&b);
+	c = a+b;
+	printf("%d + %d = %d\n", a, b, c);
+}

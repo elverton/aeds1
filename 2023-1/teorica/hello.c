e0a496a58c532cd14b8dac1edd982f3b8304163f
elverton <elvertoncf@gmail.com>
2023-04-05 11:50:28 -0300

    Aula 03/04

--- 2023-1/teorica/hello.c
+++ 2023-1/teorica/hello.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 
-int main(){
-	int a;
-	int b;
-	int c;
-	printf("Digite A: ");
-	scanf("%d",&a);
-	printf("Digite B: ");
-	scanf("%d",&b);
-	c = a+b;
-	printf("%d + %d = %d\n", a, b, c);
+int main()
+{ 
+    printf("Hello World\n"); 
+    return 0; 
 }
